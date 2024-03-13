0af54832fc211a7c0d539d84e01ab03583dcbf74
Богдан Пукас <bohdanpukas2003@gmail.com>
2024-03-13 05:31:34 +0200

    ReAdded Maui Community Toolkit. Deleted old page

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -15,10 +15,7 @@ namespace NotesKeeper
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             }).UseMauiCommunityToolkit();
-            /*builder.Services.AddSingleton<MainPage>();
-            /builder.Services.AddSingleton<NotesViewModel>();
-            builder.Services.AddSingleton<NoteDetailsPage>();
-            builder.Services.AddSingleton<NoteDetailsViewModel>();*/
+            
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
